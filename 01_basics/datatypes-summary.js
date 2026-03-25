3d763f3b4cdb3417f90e6ac52012b3695e6d069a
ritikarawat23 <ritikarawat16012001@gmail.com>
2026-03-25 17:54:31 +0000

    datatypes done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,3 +1,34 @@
 // datatypes are categorized in 2 ways : 1.how they are stored in  memory 2.how they are accessed.
 //Primitive(callbyvalue) datatype 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt(handles scientific values)
-//Reference Type(Non primitive) datatype
+ const score = 100
+ const scoreValue = 100.3 //(no scene of decimals...all float, double comes under number only)
+ const outsideTemp = null
+ let userEmail = undefined; // this variable by default will have undefined value, or we can manually define it's value as defined.
+ 
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+ console.log(id === anotherId);
+//even though we gave same symbol value(like number, string) for both variables still results for id and anotherId will not the same
+
+//bigInt example
+const bigNumber = 38475984527328139239219491209950975309n
+
+//Reference Type(Non primitive) datatype : Array, Objects, Functions
+//array
+const heros = ["shaktiman", "naagraj", "doga"]
+//object
+let obj = {
+    name:"ritika",
+    age : 25,
+}
+
+//there are many ways to declare functions..for now we are using variable way to declare function as we can treat a function as variable in javascript.
+
+const myFunction = function(){
+    console.log("HEllo World");
+}
+
+console.log(typeof outsideTemp);
+console.log(typeof obj);
+console.log(typeof myFunction);
+console.log(typeof heros);
