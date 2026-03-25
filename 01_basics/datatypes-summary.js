2c0c2edbd30b838cbc7725afe6baafbacf84afac
ritikarawat23 <ritikarawat16012001@gmail.com>
2026-03-25 16:00:50 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,3 @@
+// datatypes are categorized in 2 ways : 1.how they are stored in  memory 2.how they are accessed.
+//Primitive(callbyvalue) datatype 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt(handles scientific values)
+//Reference Type(Non primitive) datatype
