d4fc7c7e163dfce9126cb0d82931c92d904ab667
ritikarawat23 <ritikarawat16012001@gmail.com>
2026-03-27 15:52:29 +0000

    vbbm

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,7 +31,7 @@ function addTwoNumbers(number1, number2){
 }
 
 const result = addTwoNumbers(3, 5); // 3 and 4 are arguments.
-console.log("Result: ",result);
+//console.log("Result: ",result);
 
 function loginUserMessage(username = "sam"){ //here in this line we have given default value for username as sam that means if no value will be given then sam will be the default value otherwise the value passed to the function will the value.
     if(!username){
@@ -40,6 +40,33 @@ function loginUserMessage(username = "sam"){ //here in this line we have given d
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("ritika"))
+//console.log(loginUserMessage("ritika"))
 
+//rest and spread operator are same...but they are called rest or spread according to its use cases.
+function calculateCartPrice(val1, val2, ...num1){ // so here in val1 will go first value 200 then in val2 will go second value 400 and then in ...num will go rest all the values.
+    return num1
+}
+console.log(calculateCartPrice(200, 400, 500, 2000, 1000, 150, 250));
 
+const user = {
+    username: "ritika",
+    prices: 199
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.prices}`);
+}
+//can pass object like this
+//handleObject(user)
+//can pass object like this also
+handleObject({
+    username: "pragati",
+    prices: 399
+})
+
+//now let's see how to pass array
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 500, 6000, 300, 150, 250]));
