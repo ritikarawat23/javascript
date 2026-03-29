b3b5b6c36f65350620619ce130ff1e8ee312f41b
ritikarawat23 <ritikarawat16012001@gmail.com>
2026-03-29 16:03:58 +0000

    continue

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -13,3 +13,24 @@ for (const greet of greetings) {
         continue
     console.log(`each char is ${greet}`)
 }
+//map object holds key-value pairs and remembers the original insertion order of the keys.Any value can be used as either a key or a value.Key in the map may only occur once,it is unique in the map's collection.
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States Of America")
+map.set('Fr', "France")
+map.set('In', "India")
+
+//console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);  
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+for (const [key, value] of myObject) {
+    console.log(key, ":-", value);
+    
+}
